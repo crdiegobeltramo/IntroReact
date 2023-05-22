50c680bd1674602769230e708e685b8f8592d6db
crdiegobeltramo <crdiegobeltramo@gmail.com>
2023-05-22 19:40:31 -0300

    Segundo Commit

--- src/components/public/Card.js
+++ src/components/public/Card.js
@@ -4,7 +4,8 @@ export const Card = ({mascota}) => {
   return (
     <div className="col">
     <div className="card shadow-sm">
-      <img className="bd-placeholder-img card-img-top" width="100%" src= {mascota.image} alt ="imagen" />
+      <img className="bd-placeholder-img card-img-top" width="100%" height="225" src= {mascota.image} alt ="imagen" />
+      <h3 className= "mb-0 text-dark">{mascota.name} </h3>
       <div className="card-body">
         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <div className="d-flex justify-content-between align-items-center">
